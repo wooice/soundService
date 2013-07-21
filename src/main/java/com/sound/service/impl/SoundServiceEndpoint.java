47d18f75037a67882f1a112767474ba8bf8a3be3
Xiaodong, Duo <xduo@ebay.com>
2013-07-21 16:55:03 +0800

    adjust create sound service

--- src/main/java/com/sound/service/impl/SoundServiceEndpoint.java
+++ src/main/java/com/sound/service/impl/SoundServiceEndpoint.java
@@ -1,9 +1,5 @@
 package com.sound.service.impl;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -11,19 +7,14 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.sound.model.Sound;
-import com.sound.model.file.LocalFile;
 import com.sound.service.file.itf.FileService;
 import com.sound.service.sound.itf.SoundService;
-import com.sun.jersey.core.header.FormDataContentDisposition;
-import com.sun.jersey.multipart.FormDataParam;
 
 @Component
 @Path("/sound")
@@ -57,15 +48,13 @@ public class SoundServiceEndpoint {
 	
 	@PUT
 	@Path("/save")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public Response saveProfile(
-			@FormDataParam("file") InputStream uploadedInputStream,
-			@FormDataParam("file") FormDataContentDisposition fileDetail,
-			@FormDataParam("objectId") String objectId, 
-			@FormDataParam("soundAlias") String soundAlias, 
-			@FormDataParam("description") String description, 
-			@FormDataParam("ownerAlias") String ownerAlias, 
-			@FormDataParam("status") String status)
+			@FormParam("objectId") String objectId, 
+			@FormParam("soundAlias") String soundAlias, 
+			@FormParam("description") String description, 
+			@FormParam("ownerAlias") String ownerAlias, 
+			@FormParam("status") String status,
+			@FormParam("posterId") String posterId)
 	{
 
 		if (null == objectId)
@@ -78,31 +67,9 @@ public class SoundServiceEndpoint {
 			return Response.status(500).entity("sound alias can't be null").build(); 
 		}
 		
-		byte[] soundData = null;
-		try 
-		{
-			if (null != uploadedInputStream)
-			{
-				soundData = IOUtils.toByteArray(uploadedInputStream);
-			}
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-			return Response.status(500).entity("poster upload failed.").build(); 
-		}
-
-		LocalFile poster = new LocalFile();
-		poster.setContent(soundData);
-		if (null != fileDetail)
-		{
-			String[] parts = fileDetail.getName().split(".");
-			poster.setType(parts[1]);
-			poster.setFileName(parts[0]);
-		}
-		
 		try
 		{
-			soundService.saveProfile(objectId, soundAlias, description, ownerAlias, status, poster);
+			soundService.saveProfile(objectId, soundAlias, description, ownerAlias, status, posterId);
 		}
 		catch(Exception e)
 		{
